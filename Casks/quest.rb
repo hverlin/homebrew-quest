1c47625c1c9d494d9d91def90c3a6f049593fa57
hverlin <hverlin@trifacta.com>
2020-05-11 21:41:46 +0200

    add cask

---
+++ Casks/quest.rb
@@ -0,0 +1,9 @@
+cask 'quest' do
+  version "0.1.10"
+  sha256 "38f80ddbb2288f44f4b79fc4b898d919e1f85d9dba0701e814da9ceea6472fdd"
+
+  homepage "https://github.com/hverlin/Quest"
+  url "https://github.com/hverlin/Quest/releases/download/v#{version}/quest-darwin-x64-#{version}.zip"
+
+  app 'quest.app'
+end
