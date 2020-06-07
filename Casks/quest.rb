6dff991fee56bfa54266b349dc38e9be36689234
hverlin <hverlin@trifacta.com>
2020-06-07 16:23:10 +0200

    update to 0.1.13

--- Casks/quest.rb
+++ Casks/quest.rb
@@ -1,6 +1,6 @@
 cask 'quest' do
-  version "0.1.12"
-  sha256 "efc850664dd3b78d3442daf342dbf43e86466766200cfb49637f3c7c2aa89063"
+  version "0.1.13"
+  sha256 "f4dc39d24a0e38d0620a64cd200063b97bca7be486d8f9c194cab70c2f2ad344"
 
   homepage "https://github.com/hverlin/Quest"
   url "https://github.com/hverlin/Quest/releases/download/v#{version}/quest-darwin-x64-#{version}.zip"
